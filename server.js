41adf2d7172e4bafedba70bcd306acfb36f0e0f7
Breezy <pbrowning11@gmail.com>
2018-10-22 15:08:03 -0400

    jawsDB push

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var exphb = require("express-handlebars");
 var mysql = require("mysql");
 
 var app = express();
+var connection;
 
 var PORT = process.env.PORT || 8080;
 
@@ -12,13 +13,18 @@ app.use(express.json());
 app.engine("handlebars", exphb({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
+
 
 connection.connect(function (err) {
     if (err) {
