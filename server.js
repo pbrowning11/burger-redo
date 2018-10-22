c13d63ef68d5863aeaddb970be7484b893fce2cc
Breezy <pbrowning11@gmail.com>
2018-10-22 15:31:36 -0400

    heroku image

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
-app.use(express.static('public'))
+app.use(express.static('./public'))
 
 app.get("/", function (req, res) {
     connection.query("SELECT * FROM burgers", function (err, data) {
